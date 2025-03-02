2dc2b3d7524d81c7bddb54696683d9f427dfa622
Adam Dicky <admdcky@gmail.com>
2025-03-02 18:30:04 +0800

    File rewrite as there is error with Spring Security.

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,38 +1,29 @@
 package com.example.demo.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public DefaultSecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests (auth->auth
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/").permitAll()
-                        .requestMatchers("login").permitAll()
-                        .requestMatchers("/logout").permitAll()
-                        .requestMatchers("register").permitAll()
+                        .requestMatchers("/index").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/register").permitAll()
                         .anyRequest().authenticated()
                 )
-
-                .formLogin(form->form
+                .formLogin(form -> form
                         .defaultSuccessUrl("/", true)
                 )
-
-                .logout(config->config.logoutSuccessUrl("/"))
+                .logout(config -> config.logoutSuccessUrl("/"))
                 .build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/demo/repositories/userrepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.usermodel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface userrepo extends JpaRepository<usermodel, Integer> {
-    public usermodel findByUsername(String username);
-}
