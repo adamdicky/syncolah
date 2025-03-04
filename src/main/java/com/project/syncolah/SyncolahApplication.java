9c3082782f9a49ba00aaefbf0b54dec77cfbf67c
Adam Dicky <admdcky@gmail.com>
2025-03-04 16:16:01 +0800

    Init Syncolah

---
+++ src/main/java/com/project/syncolah/SyncolahApplication.java
@@ -0,0 +1,13 @@
+package com.project.syncolah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncolahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncolahApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/syncolah/SyncolahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.syncolah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncolahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
